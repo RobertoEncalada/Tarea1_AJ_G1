72383ba3b60f3a8f1b834fbfb086367f430403fc
Christopher Acosta <chriacos@espol.edu.ec>
2022-10-28 21:10:08 -0500

    Controlador para botones

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,13 +9,15 @@ import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			
 			Parent root= FXMLLoader.load(getClass().getResource("Primario.fxml"));
 			Scene scene= new Scene(root);  
-
+			
 			
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- src/application/PrimarioController.java
+++ src/application/PrimarioController.java
@@ -1,43 +1,62 @@
 package application;
 
-
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
-
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
-
-
-import javafx.event.ActionEvent;
 
 import javafx.scene.layout.Pane;
 
-public class PrimarioController {
+public class PrimarioController implements Initializable {
 	@FXML
 	private Pane fondo;
 	@FXML
 	private Button botonRojo;
 	@FXML
+	private Button botonAmarillo;
+	@FXML
 	private Button botonVerde;
 	@FXML
-	private Button botonAzul;
-
-	// Event Listener on Button[#botonRojo].onAction
-	@FXML
-	public void cambioArojo(ActionEvent event) {
-
+	private Button botonAzar;
+	
+	private Parent root;
+	
+	private void cambiarArojo(Parent root) {}
+	private void cambiarAamarillo(Parent root) {}
+	private void cambiarAverde(Parent root) {}
+	private void cambiarAazar(Parent root, boolean b) {}
+	Boolean keepMixing = false;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		botonRojo.setOnAction(eh -> {
+            Node source = (Node) eh.getSource();
+            this.root = (Parent) source.getScene().getRoot();
+            cambiarArojo(root);
+        });
 		
+		botonAmarillo.setOnAction(eh -> {
+            Node source = (Node) eh.getSource();
+            this.root = (Parent) source.getScene().getRoot();
+            cambiarAamarillo(root);
+        });
+		
+		botonVerde.setOnAction(eh -> {
+            Node source = (Node) eh.getSource();
+            this.root = (Parent) source.getScene().getRoot();
+            cambiarAverde(root);
+        });
+		
+		botonAzar.setOnAction(eh -> {
+            Node source = (Node) eh.getSource();
+            this.root = (Parent) source.getScene().getRoot();
+            cambiarAazar(root, keepMixing);
+        });
 	}
-	// Event Listener on Button[#botonVerde].onAction
-	@FXML
-	public void cambioAverde(ActionEvent event) {
-		
 
-	}
-	// Event Listener on Button[#botonAzul].onAction
-	@FXML
-	public void cambioAazul(ActionEvent event) {
-
-		
-		
-	}
+	
 }
